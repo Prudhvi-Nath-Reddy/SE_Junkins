412be828b3583cb2fbb07f52db6d00bdf17c9378
Prudhvi-Nath-Reddy <95532151+Prudhvi-Nath-Reddy@users.noreply.github.com>
2022-11-25 03:21:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ from cube import cube
 
 class test_code(unittest.TestCase):
 
-    def sq_test(self):
+    def test_sq_test(self):
         n = 2
         result = square(n)
         self.assertEqual(result, 5)
 
-    def cube_test(self):
+    def test_cube_test(self):
         n = 2
         result = cube(n)
         self.assertEqual(result, 900)
