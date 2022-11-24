fbb1eab468836460e458cd431be99fcf365f4d04
Prudhvi-Nath-Reddy <95532151+Prudhvi-Nath-Reddy@users.noreply.github.com>
2022-11-25 03:27:52 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ import unittest
 from square import square
 from cube import cube
 
-class test_code(unittest.TestCase):
+class test(unittest.TestCase):
 
-    def test_sq_test(self):
+    def sq_test(self):
         n = 2
         result = square(n)
         self.assertEqual(result, 4)
 
-    def test_cube_test(self):
+    def cube_test(self):
         n = 2
         result = cube(n)
-        self.assertEqual(result, 90)
+        self.assertEqual(result, 8)
 
 
 if __name__ == '__main__':
